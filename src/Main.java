902575ed10c35007fe3412eab89fccbbe5a5eb45
Kateryna2882 <111122172+Kateryna2882@users.noreply.github.com>
2023-10-24 21:57:56 +0300

    Add fields

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 
 public class Main {
     public static void main(String[] args) {
+        Solution solution = new Solution();
+        String input1 = "A man, a plan, a canal : Panama";
+        System.out.println("Input : " + input1);
+        System.out.println("Output : " + solution.isPalindrome(input1));
+        String input2 = "race a car";
+        System.out.println("Input : " +input2);
+        System.out.println("Output : " + solution.isPalindrome(input2));
+        String input3 = " ";
+        System.out.println("Input : " + input3);
+        System.out.println("Output : " + solution.isPalindrome(input3));
     }
 }
--- src/task2/ContainsDuplicate.java
+++ src/task2/ContainsDuplicate.java
@@ -1,4 +1,18 @@
 package task2;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+
+        Set<Integer> seen = new HashSet<>();
+        for (int number : nums) {
+            if (seen.contains(number)) {
+                return true;
+            }
+            seen.add(number);
+        }
+        return false;
+    }
 }
